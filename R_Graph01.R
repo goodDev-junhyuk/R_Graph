e76e3f2e4c736959f7b853e42e6c71c89360589e
Jang Jun Hyuk <dejavukk1@gmail.com>
2020-12-08 16:11:25 +0900

    Add files via upload
    산점도 그래프, x, y축 범위 지정.

---
+++ R_Graph01.R
@@ -0,0 +1,21 @@
+library(ggplot2)
+
+install.packages("dplyr")
+library(dplyr)
+
+# x축은 displ, y축은 hwy로 지정해 배경 생성.
+ggplot(data = mpg, aes(x = displ, y = hwy))
+
+# 배경에 산점도 추가
+ggplot(data = mpg, aes(x = displ, y = hwy)) + geom_point()
+
+# x축 범위 3~6으로 지정
+ggplot(data = mpg, aes(x = displ, y = hwy)) + 
+    geom_point() + 
+    xlim(3, 6)
+# x축 범위, y축 범위 10~30으로 지정.
+ggplot(data = mpg, aes(x = displ, y = hwy)) + 
+    geom_point() + 
+    xlim(3, 6) + 
+    ylim(10, 30)
+
